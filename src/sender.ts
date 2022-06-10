c5bff65a9b0c6c137a8ed73c3cb695a653d0f31e
Jonatan Machado <jonatanmachado@gmail.com>
2022-06-10 14:26:42 -0300

    first commit

---
+++ src/sender.ts
@@ -0,0 +1,27 @@
+import {create, Whatsapp, Message, SocketState} from 'venom-bot'
+
+class Sender {
+  private client: Whatsapp
+
+  constructor(){
+    this.initialize()
+  }
+
+  private initialize(){
+    const qr = (base64Qrimg: string) =>{}
+
+    const status = (statusSession:string) =>{
+
+    }
+
+    const start = (client: Whatsapp)=>{
+      this.client = client
+    }
+
+    create('ws-sender-dev', qr, status)
+      .then((client)=> start(client))
+      .catch((error)=> console.error(error))
+  }
+}
+
+export default Sender
