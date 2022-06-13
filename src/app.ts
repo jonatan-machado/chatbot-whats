55115430c7b9e4aa40067caae446c4fcef035d7f
Jonatan Machado <jonatanmachado@gmail.com>
2022-06-12 22:56:24 -0300

    bot alternativo

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,68 @@
-import express,{Request, Response} from 'express'
-import Sender from './sender'
+import { create,Whatsapp } from 'venom-bot';
 
-const sender = new Sender()
+create({session:'ola',multidevice:true})
+  .then((client)=>start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-const app = express()
+  function start(client:Whatsapp) {
+    let stage = 0
+    let data:any ={}
+    let localidade:string
+    let price:number
+    let dormitorio:string
+    client.onMessage((message:any) => {
+      if(String(message.body).toUpperCase() === 'SAIR' 
+        || String(message.body).toUpperCase() === 'PARAR' 
+        || String(message.body).toUpperCase() === 'STOP'){
+          client.sendText(message.from,'Ok, atendimento encerrado, Caso queira voltar só me chamar =)')
+          return
+        }
+        if(stage === 0){
+          client.sendText(message.from,`Olá ${message.sender.name}, bem vindo a dacas, vi que gostou do produto xyz, poderia me informar qual localização está buscando?`)
+          
+          stage+=1
+          return
+        }
+        if(stage === 1){
+          localidade = message.body
+          client.sendText(message.from,`ok, vi que busca na localidade ${localidade}
+          poderia me informar quantos dormitorios procura?
+          `)
+          stage+=1
+          console.log('localidade',message.text)
+          return
+          
+        }
+        if(stage === 2){
+          dormitorio = message.text
+          client.sendText(message.from,`ok, vi que busca na localidade ${localidade} e com ${dormitorio} dormitorio(s)
+          poderia me informar até quanto quer investir?
+          `)
+          stage+=1
+          console.log('dormitorio',message.text)
+          return
+        }
+        if(stage === 3){
+          price = message.body
+          client.sendText(message.from,`ok, você busca na localidade ${localidade}, com ${dormitorio} dormitorio(s)
+          e quer investir R$ ${price}, um dos nossos especialistas estará conduzindo melhor a conversa, agora
+          `)
+          console.log('price',message.body)
+          console.log(price,dormitorio,localidade)
+          stage+=1
+          return
+        }
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-app.get('/status',(req:Request, res:Response)=> {
-  return res.send({
-    qr_code: sender.qrCode,
-    connected: sender.isConnected
-  })
-})
-
-app.post('/send', async(req:Request, res:Response)=>{
-  const {number, message} = req.body
-
-  try {
-    //validar e transformar o numero do wpp
-
-
-    await sender.sendText(number,message)
-
-    return res.status(200).json()
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({status: "error", message:error})
+        data = {
+          price:price,
+          dormitorio:dormitorio,
+          localidade:localidade
+        }
+        console.log(data)
+      
+    });
   }
-})
 
-app.listen(3333,()=>{
-  console.log('server started')
-})
+  export default start
